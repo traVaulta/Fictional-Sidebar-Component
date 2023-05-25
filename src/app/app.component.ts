3bcd94f52d73f6306e4e299a11dc26628ba76361
Matija Čvrk <matijacvrk@gmail.com>
2023-05-25 17:55:49 +0200

    static - styles - added initial sidebar component with hardcoded data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 @Component({
   selector: 'orfs-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SidebarComponent
+  ],
   template: `
-    <div></div>
+    <div>
+      <div class="sidebar-heading fg-primary-900 w-700">Companies</div>
+      <orfs-sidebar></orfs-sidebar>
+    </div>
   `,
-  styles: [],
+  styles: [`
+    .sidebar-heading {
+      margin: 14px 0;
+    }
+  `],
 })
 export class AppComponent {}
