01903b1337fbec2fd58e7e176ddef8e650f25e2a
Matija Čvrk <matijacvrk@gmail.com>
2023-05-25 20:40:14 +0200

    transform - removed hardcoded template

--- src/app/sidebar/content/content.component.ts
+++ src/app/sidebar/content/content.component.ts
@@ -4,7 +4,7 @@ import { Component, computed, OnInit, signal } from '@angular/core';
 import { LinkIconComponent } from '../../icons/link-icon/link-icon.component';
 import { ClientService } from './client.service';
 import { prepEmployee, prepItems } from './mapper';
-import { DepartmentDto, EmployeeDto, EmployeeUI, ResponseDto } from './models';
+import { DepartmentDto, EmployeeDto, ResponseDto } from './models';
 
 @Component({
   standalone: true,
@@ -15,56 +15,22 @@ import { DepartmentDto, EmployeeDto, EmployeeUI, ResponseDto } from './models';
   ],
   template: `
     <div class="sidebar-heading fg-primary-900 w-700">Companies</div>
-    <nav class="fg-primary-900 w-700">
-      Ravvio
-      <ul class="no-decoration fg-primary-900 w-700">
-        <li>
-          Finance Team
-          <ul class="no-decoration fg-grey w-600">
-            <li class="flex space-between">Michael P. Lucifer <orfs-link-icon></orfs-link-icon></li>
-          </ul>
-        </li>
-        <li>
-          Technical Team
-          <ul class="no-decoration fg-grey w-600">
-            <li class="flex space-between">Marlyn B. Brown <orfs-link-icon></orfs-link-icon></li>
-            <li class="flex space-between">David P. Perez <orfs-link-icon></orfs-link-icon></li>
-          </ul>
-         </li>
-      </ul>
-    </nav>
-    <nav class="fg-primary-900 w-700">
-      CashLab
-      <ul class="no-decoration fg-grey w-600">
-        <li class="flex space-between">Anna J. Kelly <orfs-link-icon></orfs-link-icon></li>
-        <li class="flex space-between">Brenda J. Soto <orfs-link-icon></orfs-link-icon></li>
-      </ul>
-    </nav>
-    <nav class="fg-primary-900 w-700">
-      Dovish
-      <ul class="no-decoration fg-primary-900 w-700">
-        <li>
-          Management
-          <ul class="no-decoration fg-grey w-600">
-            <li class="flex space-between">Timothy A. Merrow <orfs-link-icon></orfs-link-icon></li>
-          </ul>
-         </li>
-      </ul>
-    </nav>
-    <hr>
-    <hr>
-    <hr>
     <nav *ngFor="let company of result()" class="fg-primary-900 w-700">
       {{ company.name }}
-      <ul class="no-decoration fg-primary-900 w-700">
-        <li *ngFor="let depOrEmpl of asAny(company.items)">
-          {{ depOrEmpl.name }}
-          <ng-container *ngIf="extractIfDepartment(depOrEmpl) as empls">
-            <ul *ngFor="let empl of empls" class="no-decoration fg-grey w-600">
-              <li class="flex space-between">{{ empl.name }} <orfs-link-icon></orfs-link-icon></li>
+      <ul class="no-decoration">
+        <ng-container *ngFor="let depOrEmpl of asAny(company.items)">
+          <ng-template #noDeps>
+            <li class="fg-grey w-600 flex space-between">{{ depOrEmpl.name }}</li>
+          </ng-template>
+          <ng-container *ngIf="extractIfDepartment(depOrEmpl) as empls; else noDeps">
+            <li [ngClass]="{ 'fg-primary-900 w-700': empls.length > 0, 'fg-grey w-600': empls.length < 1 }">
+                {{ depOrEmpl.name }}
+            </li>
+            <ul *ngFor="let empl of empls" class="no-decoration">
+              <li class="fg-grey w-600 flex space-between">{{ empl.name }} <orfs-link-icon></orfs-link-icon></li>
             </ul>
           </ng-container>
-        </li>
+        </ng-container>
       </ul>
     </nav>
   `
