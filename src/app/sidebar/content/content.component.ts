3ea3fcc49f11db1f48529522838ab044ed544475
Matija Čvrk <matijacvrk@gmail.com>
2023-05-30 20:13:19 +0200

    transform - moved client service to proper domain folder

--- src/app/sidebar/content/content.component.ts
+++ src/app/sidebar/content/content.component.ts
@@ -3,10 +3,10 @@ import { Component, computed, OnInit, signal } from '@angular/core';
 
 import { IconDirective } from '../../icons/icon.directive';
 import { LinkIconComponent } from '../../icons/link-icon/link-icon.component';
-import { ClientService } from '../../../domain/client.service';
 import { mapResponseRecursively } from '../../../domain/company/mapper';
-import { EmployeeDto, ResponseDto } from '../../../domain/employee/model';
 import { DepartmentDto } from '../../../domain/department/model';
+import { ClientService } from '../../../domain/employee/client';
+import { EmployeeDto, ResponseDto } from '../../../domain/employee/model';
 
 @Component({
   standalone: true,
--- src/domain/employee/client.ts
+++ src/domain/employee/client.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
-import { ResponseDto } from './employee/model';
+import { ResponseDto } from './model';
 
 @Injectable({ providedIn: 'root' })
 export class ClientService {
