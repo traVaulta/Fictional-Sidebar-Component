b410c15663b43b82f2e6bac17365d80933b739f4
Matija Čvrk <matijacvrk@gmail.com>
2023-05-30 20:07:20 +0200

    transform - provided http service

--- src/domain/client.service.ts
+++ src/domain/client.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { ResponseDto } from './models';
 
 @Injectable({ providedIn: 'root' })
 export class ClientService {
+  constructor(private http: HttpClient) {}
+
   async getAll(): Promise<ResponseDto[]> {
-    return await (await fetch('/assets/data.json')).json();
+    return (await this.http.get<ResponseDto[]>('/assets/data.json').toPromise() ?? []);
   }
 }
