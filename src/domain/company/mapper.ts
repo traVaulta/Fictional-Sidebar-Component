541ebde32576227fd82e71132d75e38ff4416b5f
Matija Čvrk <matijacvrk@gmail.com>
2024-01-05 08:11:09 +0100

    corrected mapper

--- src/domain/company/mapper.ts
+++ src/domain/company/mapper.ts
@@ -3,35 +3,27 @@ import { DepartmentDto } from '../department/model';
 import { CompanyDto } from './model';
 
 export const mapData = (response: ResponseDto[]) => {
-  const companies: { [k: string]: CompanyDto } = {};
-  const departments: { [k: string]: DepartmentDto } = {};
-  const employees: { [k: string]: EmployeeDto } = {};
+  const companies: CompanyDto[] = [];
 
-  for (const item of response) {
-    const segments = item.name.split('/');
-
-    const department: DepartmentDto | undefined = segments.length < 3 ? undefined : <DepartmentDto>{ name: segments[1], items: [] };
-    const company: CompanyDto | undefined = <CompanyDto>{ name: segments[0], items: [] };
-    const employee: EmployeeDto | undefined = <EmployeeDto>{ name: segments.length < 3 ? segments[1] : segments[2] };
-
-    if (!companies[company.name]) companies[company.name] = company;
-    if (department && !departments[department.name]) departments[department.name] = department;
-    if (!employees[employee.name]) employees[employee.name] = employee;
-
-    let existingEmployee = employees[employee.name]!;
-    let existingDepartment: DepartmentDto | undefined;
-    if (department && (existingDepartment = departments[department.name])) {
-      existingDepartment.items.push(existingEmployee);
+  for (let i = 0; i < response.length; i++) {
+    const item = response[i];
+    const { id } = item;
+    let [name, ...rest] = item.name.split(' / ');
+    let companyId = companies.findIndex(c => c.name === name);
+    if (companyId < 0) {
+      companies.push({ name, items: [] });
+      companyId = companies.length - 1;
     }
-    let existingCompany: CompanyDto | undefined;
-    if ((existingCompany = companies[company.name]) && existingDepartment) {
-      const existing = <DepartmentDto[]>existingCompany.items;
-      existingCompany.items = [...existing, existingDepartment];
-    } else if (existingCompany) {
-      const existing = <EmployeeDto[]>existingCompany.items;
-      existingCompany.items = [...existing, existingEmployee];
+    [name, ...rest] = rest;
+    const department = name && rest?.length ? (<DepartmentDto[]>companies[companyId].items).find(d => d.name === name) : undefined;
+    if (!department && name && rest?.length) {
+      (<DepartmentDto[]>companies[companyId].items).push({ name, items: [ { name: rest[0], id } ] });
+    } else if (!department && name) {
+      (<EmployeeDto[]>companies[companyId].items).push({ name, id });
+    } else if (department && name && rest?.length && !(<EmployeeDto[]>department.items).some(e => e.id === id)) {
+      (<EmployeeDto[]>department.items).push({ name, id });
     }
   }
 
-  return Object.values(companies);
+  return companies;
 };
